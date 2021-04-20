8f79a3ca0065d082cf9bb19daecc445f49999e5d
Ido Frenkel <Ido123net@gmail.com>
2021-04-20 16:09:57 +0300

    start to work in main functions

--- Systems programming lab/mmn12/q1/myText.c
+++ Systems programming lab/mmn12/q1/myText.c
@@ -1,2 +1,23 @@
+#include <stdio.h>
+#include "myText.h"
 
+typedef enum {buffer, linkedList} data_struct;
 
+int readText(data_struct s, void *data)
+{
+    int c;
+    char line[size];
+    while ((c = getchar()) != EOF)
+    {
+        switch (s)
+        {
+        case /* constant-expression */:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+}
